e17e7634cf215feec909c82ca133bbe33be62ccf
DevThomas <fremen1990@gmail.com>
2022-09-28 18:56:31 +0200

    Token Cleanup

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const app = require('./src/app');
 const sequelize = require('./src/config/database');
 const User = require('./src/user/User');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const TokenService = require('./src/auth/TokenService');
 
 const addUsers = async (activeUserCount, inactiveUserCount = 0) => {
-  const hash = await bcrypt.hash("P4ssword",10)
+  const hash = await bcrypt.hash('P4ssword', 10);
   for (let i = 0; i < activeUserCount + inactiveUserCount; i++) {
     await User.create({
       username: `user${i + 1}`,
       email: `user${i + 1}@mail.com`,
       inactive: i >= activeUserCount,
-      password: hash
+      password: hash,
     });
   }
 };
@@ -18,5 +19,7 @@ const addUsers = async (activeUserCount, inactiveUserCount = 0) => {
 sequelize.sync({ force: true }).then(async () => {
   await addUsers(25);
 });
+
+TokenService.scheduleCleanup();
 
 app.listen(3000, () => console.log('server is running'));
--- src/auth/TokenService.js
+++ src/auth/TokenService.js
@@ -2,6 +2,7 @@
 const { randomString } = require('../shared/generator');
 const Token = require('./Token');
 const { Sequelize } = require('sequelize');
+const ONE_WEEK_IN_MILLIS = 7 * 24 * 60 * 60 * 1000;
 
 const createToken = async (user) => {
   const token = randomString(32);
@@ -15,7 +16,7 @@ const createToken = async (user) => {
 };
 
 const verify = async (token) => {
-  const oneWeekAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000);
+  const oneWeekAgo = new Date(Date.now() - ONE_WEEK_IN_MILLIS);
   const tokenInDB = await Token.findOne({
     where: {
       token: token,
@@ -34,5 +35,17 @@ const verify = async (token) => {
 const deleteToken = async (token) => {
   await Token.destroy({ where: { token: token } });
 };
+const scheduleCleanup = () => {
+  setInterval(async () => {
+    const oneWeekAgo = new Date(Date.now() - ONE_WEEK_IN_MILLIS);
+    await Token.destroy({
+      where: {
+        lastUsedAt: {
+          [Sequelize.Op.lt]: oneWeekAgo,
+        },
+      },
+    });
+  }, 60 * 60 * 1000);
+};
 
-module.exports = { createToken, verify, deleteToken };
+module.exports = { createToken, verify, deleteToken, scheduleCleanup };
