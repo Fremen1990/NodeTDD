4435ae75e2864fced7e95dfe917aebff68bd76dd
DevThomas <fremen1990@gmail.com>
2022-10-04 09:41:34 +0200

    File Size Validation

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ const app = express();
 
 app.use(middleware.handle(i18next));
 
-app.use(express.json());
+app.use(express.json({ limit: '3mb' }));
 
 app.use('/images', express.static(profileFolder, { maxAge: ONE_YEAR_IN_MILLIS }));
 
