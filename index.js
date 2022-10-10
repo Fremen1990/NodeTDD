c6acd25cb80e17a3d4ad897a328df1a1a74f67f5
DevThomas <fremen1990@gmail.com>
2022-10-10 02:19:55 +0200

    Logging App Version

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ sequelize.sync();
 
 TokenService.scheduleCleanup();
 
-logger.error('error');
-logger.warn('warn');
-logger.info('info');
-logger.verbose('verbose');
-logger.debug('debug');
-logger.silly('silly');
+// logger.error('error');
+// logger.warn('warn');
+// logger.info('info');
+// logger.verbose('verbose');
+// logger.debug('debug');
+// logger.silly('silly');
 
-app.listen(3000, () => logger.info('server is running'));
+app.listen(3000, () => logger.info('server is running. Version: ' + process.env.npm_package_version));
