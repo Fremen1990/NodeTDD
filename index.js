ebe4edd37ce92d7c3f73edfd2f890d535a9389d1
DevThomas <fremen1990@gmail.com>
2022-10-10 02:16:53 +0200

    Logger

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const app = require('./src/app');
 const sequelize = require('./src/config/database');
 const TokenService = require('./src/auth/TokenService');
+const logger = require('./src/shared/logger');
 
 sequelize.sync();
 
 TokenService.scheduleCleanup();
 
-app.listen(3000, () => console.log('server is running'));
+logger.error('error');
+logger.warn('warn');
+logger.info('info');
+logger.verbose('verbose');
+logger.debug('debug');
+logger.silly('silly');
+
+app.listen(3000, () => logger.info('server is running'));
--- src/email/EmailService.js
+++ src/email/EmailService.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
 const transporter = require('../config/emailTransporter');
+const logger = require('../shared/logger');
 
 const sendAccountActivation = async (email, token) => {
   const info = await transporter.sendMail({
@@ -16,8 +17,8 @@ const sendAccountActivation = async (email, token) => {
 </div>`,
   });
 
-  if (process.env.NODE_ENV === 'dev') {
-    console.log('url: ' + nodemailer.getTestMessageUrl(info));
+  if (process.env.NODE_ENV === 'development') {
+    logger.info('url: ' + nodemailer.getTestMessageUrl(info));
   }
 };
 
@@ -36,8 +37,8 @@ const sendPasswordReset = async (email, token) => {
 </div>`,
   });
 
-  if (process.env.NODE_ENV === 'dev') {
-    console.log('url: ' + nodemailer.getTestMessageUrl(info));
+  if (process.env.NODE_ENV === 'development') {
+    logger.info('url: ' + nodemailer.getTestMessageUrl(info));
   }
 };
 
