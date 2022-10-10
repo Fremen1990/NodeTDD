32fca5377a716bf849aced0d2e61fa2f37b99132
DevThomas <fremen1990@gmail.com>
2022-10-10 22:25:41 +0200

    cleaning unnessesary comments

--- __tests__/Auth.spec.js
+++ __tests__/Auth.spec.js
@@ -7,7 +7,6 @@ const en = require('../locales/en/translation.json');
 const pl = require('../locales/pl/translation.json');
 const Token = require('../src/auth/Token');
 
-//TEST TRIGGER DEPLOYMENT
 beforeAll(async () => {
   if (process.env.NODE_ENV === 'test') {
     await sequelize.sync();
