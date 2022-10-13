51607bfbf57808fd8895ab25ec2434e356d35462
DevThomas <fremen1990@gmail.com>
2022-10-13 22:27:47 +0200

    Listing Hoaxes of a User

--- src/hoax/HoaxService.js
+++ src/hoax/HoaxService.js
@@ -1,6 +1,7 @@
 const Hoax = require('./Hoax');
 const { Sequelize } = require('sequelize');
 const User = require('../user/User');
+const NotFoundException = require('../error/NotFoundException');
 
 const save = async (body, user) => {
   const hoax = {
@@ -11,13 +12,22 @@ const save = async (body, user) => {
   await Hoax.create(hoax);
 };
 
-const getHoaxes = async (page, size) => {
+const getHoaxes = async (page, size, userId) => {
+  let where = {};
+  if (userId) {
+    const user = await User.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new NotFoundException('user_not_found');
+    }
+    where = { id: userId };
+  }
   const hoaxesWithCount = await Hoax.findAndCountAll({
     attributes: ['id', 'content', 'timestamp'],
     include: {
       model: User,
       as: 'user',
       attributes: ['id', 'username', 'email', 'image'],
+      where: where,
     },
     order: [['id', 'DESC']],
     limit: size,
