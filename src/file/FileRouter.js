b7577c026c8848048ce1129580b28e3d9e15d45a
DevThomas <fremen1990@gmail.com>
2022-10-14 00:07:09 +0200

    Storing Attached File

--- src/file/FileRouter.js
+++ src/file/FileRouter.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const FileService = require('./FileService');
+const multer = require('multer');
+const upload = multer();
 
-router.post('/api/1.0/hoaxes/attachments', async (req, res) => {
-  await FileService.saveAttachment();
+router.post('/api/1.0/hoaxes/attachments', upload.single('file'), async (req, res) => {
+  // console.log(req.file);
+  await FileService.saveAttachment(req.file);
   res.send();
 });
 
