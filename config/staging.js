1c239de8c396436af4f138bfc38bd1cdd18584a6
DevThomas <fremen1990@gmail.com>
2022-10-14 15:22:50 +0200

    staging config change to sqlite

--- config/staging.js
+++ config/staging.js
@@ -1,14 +1,14 @@
 module.exports = {
   database: {
     database: 'hoaxify',
-    // username: 'my-db-user',
-    username: 'postgres',
-    // password: 'db-pwd',
-    password: 'postgres',
-    // dialect: 'sqlite',
-    dialect: 'postgres',
-    // storage: './staging.sqlite',
-    host: 'localhost', // postgres purpose
+    username: 'my-db-user',
+    // username: 'postgres',
+    password: 'db-pwd',
+    // password: 'postgres',
+    dialect: 'sqlite',
+    // dialect: 'postgres',
+    storage: './staging.sqlite',
+    // host: 'localhost', // postgres purpose
     logging: false,
   },
   mail: {
